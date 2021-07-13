9306f1fc2596bb34b03cb14d22cab9e39ef268c5
bel11 <67809754+bel11@users.noreply.github.com>
2021-07-12 20:03:26 -0400

    log in/out, sign up - merge (#2)
    * initial commit
    
    * added log in/out and sign up
    
    can log in/out
    can sign up
    setup Parse as backend
    
    * deleted ExampleUnitTest.java
    
    deleted ExampleUnitTest.java
    
    Co-authored-by: Maggie Barbagallo <32804469+mbarbagallo@users.noreply.github.com>

---
+++ app/src/main/java/com/example/travelapp/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.example.travelapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Register parse models here - ParseObject.registerSubclass(ClassName.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("AxIxYefyJfSCMZBUvkf9C6mAMws6kcZ1rwaquh3L")
+                .clientKey("BsmUUhD7JEH5tivoat5nnAANOYvglYpKok0ysmx9")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
