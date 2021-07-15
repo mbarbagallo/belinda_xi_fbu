14532acb319bb213de2fe5b9380a7e5e1286f4aa
bel11 <67809754+bel11@users.noreply.github.com>
2021-07-15 18:03:34 -0400

    can get driving distance between locations
    can get driving distance from an origin to a destination
    fixed things pointed out during code review this morning (ex - single element array)

--- app/src/main/java/com/example/travelapp/Itinerary.java
+++ app/src/main/java/com/example/travelapp/Itinerary.java
@@ -1,5 +1,12 @@
 package com.example.travelapp;
 
+import android.util.Log;
+
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.Distance;
 import com.parse.ParseObject;
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
@@ -16,6 +23,8 @@ public class Itinerary extends ParseObject{
     public static final String KEY_LOCATIONS = "locations";
     public static final String KEY_IDS = "ids";
     public static final String KEY_USER = "user";
+    private static final String TAG = "Itinerary";
+    private Distance distance = new Distance();
 
     public String getLocations() {
         return getString(KEY_LOCATIONS);
@@ -35,5 +44,27 @@ public class Itinerary extends ParseObject{
 
     public void setIds(List<String> ids) {
         put(KEY_IDS, ids);
+    }
+
+    public Distance getDistance(String id1, String id2, GeoApiContext mGeoApiContext) {
+
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        directions.origin("place_id:" + id1);
+        directions.destination("place_id:" + id2).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                // TODO - for possibly considering routes & duration too later
+//                Log.d(TAG, "calculateDirections: routes: " + result.routes[0].toString());
+//                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].duration);
+                Log.d(TAG, "calculateDirections: distance: " + result.routes[0].legs[0].distance);
+                distance = result.routes[0].legs[0].distance;
+            }
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "calculateDirections: Failed to get directions: " + e.getMessage() );
+            }
+        });
+        return distance;
     }
 }
