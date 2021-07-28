97625d5fcd441937e023f45b538df0d51b9af8bf
bel11 <67809754+bel11@users.noreply.github.com>
2021-07-28 19:30:30 -0400

    when itinerary submitted, takes user to home page
    when itinerary submitted, takes user to home page automatically
    fixed minor error in binary min heap file (had instantiated the same variable twice)

--- app/src/main/java/com/example/travelapp/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/travelapp/fragments/ComposeFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -220,6 +221,7 @@ public class ComposeFragment extends Fragment {
                 ids.clear();
                 moreItemsAdapter.notifyDataSetChanged();
                 mainActivity.hideProgressBar();
+                mainActivity.switchToHomeFragment();
             }
         });
         return itinerary;
